0d0c483f18da08f25f6c94efdd72f10ed7f7e18d
aelseewy <74848911+aelseewy@users.noreply.github.com>
2020-12-27 20:30:03 +0200

    test for adding apps

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'carzone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carzone/static'),
+]
+
+
+# Media Folder Setting
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ pages/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    template = 'pages/home.html'
+    return render(request, template)
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+def cars(request):
+    template = 'cars/cars.html'
+    return render(request, template)
+
+def services(request):
+    template = 'pages/services.html'
+    return render(request, template)
+
+def contact(request):
+    template = 'pages/contact.html'
+    return render(request, template)
